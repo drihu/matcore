34ca8a80cf5d0ecef650238ef2f2c7c451f56c96
Ricardo Huamani <drihupp@gmail.com>
2017-05-23 05:50:22 -0500

    Add username length verification

--- app/controllers/auth/signUpController.js
+++ app/controllers/auth/signUpController.js
@@ -1,37 +1,51 @@
 'use strict';
 let User = require('../../models/user');
 
-let signUp = function (req, res, next) {
-  let username = req.body.username;
-  let password = req.body.password;
-  let passwordAgain = req.body.passwordAgain;
+let signUp = [
+  function (req, res, next) {
+    let username = req.body.username;
+    let signUpView = req.app.locals.config.views.signUp;
 
-  let signUpView = req.app.locals.config.views.signUp;
-
-  User.findOne({ username: req.body.username }, (err, user) => {
-    if (user) {
-      let alert = 'That user already exists.';
-      res.render(signUpView, { title: 'Sign Up',  alert: alert });
+    if (username.length < 6) {
+      let alert = 'Your username must be at least six characters.';
+      res.render(signUpView, { title: 'Sign Up', alert: alert });
 
     } else {
-      if (password === passwordAgain) {
-        let user = new User({
-          username: username,
-          password: password,
-        });
+      next();
+    }
+  },
 
-        user.save();
+  function (req, res, next) {
+    let username = req.body.username;
+    let password = req.body.password;
+    let passwordAgain = req.body.passwordAgain;
+    let signUpView = req.app.locals.config.views.signUp;
 
-        let alert = 'Your user has been created.';
-        res.render(signUpView, { title: 'Sign Up', alert: alert });
+    User.findOne({ username: req.body.username }, (err, user) => {
+      if (user) {
+        let alert = 'That user already exists.';
+        res.render(signUpView, { title: 'Sign Up',  alert: alert });
 
       } else {
-        let alert = 'Passwords do not match.';
-        res.render(signUpView, { title: 'Sign Up', alert: alert });
+        if (password === passwordAgain) {
+          let user = new User({
+            username: username,
+            password: password,
+          });
+
+          user.save();
+
+          let alert = 'Your user has been created.';
+          res.render(signUpView, { title: 'Sign Up', alert: alert });
+
+        } else {
+          let alert = 'Passwords do not match.';
+          res.render(signUpView, { title: 'Sign Up', alert: alert });
+        }
       }
-    }
-  });
-};
+    });
+  }
+];
 
 
 module.exports = signUp;
