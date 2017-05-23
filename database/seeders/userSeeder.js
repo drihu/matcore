d63e2587973e1f811c131db9b3cb9354eebaaaa6
Ricardo Huamani <drihupp@gmail.com>
2017-05-23 05:32:23 -0500

    Create userSeeder.js

---
+++ database/seeders/userSeeder.js
@@ -0,0 +1,19 @@
+'use strict';
+require('dotenv').config({ path: __dirname + '/../../.env' });
+let mongoose = require('../../config/mongoose');
+let User = require('../../app/models/user');
+
+let user = new User({
+  username: 'username',
+  password: 'password',
+});
+
+user.save((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('New added user');
+  }
+
+  process.exit();
+});
